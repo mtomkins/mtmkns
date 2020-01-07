78826ac99d74eb98f940985e589c7b6ebae8cc12
mtomkins <michaeltomkins@gmail.com>
2020-01-06 20:33:51 -0800

    Delete main.js

--- main.js
+++
@@ -1,7 +0,0 @@
-const about = document.querySelector(".aboutNav");
-const work = document.querySelector(".workNav");
-const contact = document.querySelector(".contactNav");
-
-const tl = new TimelineMax();
-
-tl.fromTo(work, 1, { height: "100%" }, { height: "50%" });
